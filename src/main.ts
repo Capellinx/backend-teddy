e6326ecedfdb43660f1e1399cd49b50a5a1ded09
capellinx <capellaaa7@gmail.com>
2024-12-07 19:16:11 -0300

    feat: use config variable validate with zod

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ConfigService } from "@nestjs/config";
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Env } from "./env";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  const config = app.get<ConfigService<Env, true>>(ConfigService);
+  const port = config.get<Env>('PORT', { infer: true});
+  
+  await app.listen(port);
 }
 bootstrap();
