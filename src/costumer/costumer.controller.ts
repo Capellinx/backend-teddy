555d58fdea940f1fe696684e05f87e930a37d1ce
capellinx <capellaaa7@gmail.com>
2024-12-07 20:25:10 -0300

    feat: create controller to connect repository

---
+++ src/costumer/costumer.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { CostumerService } from './costumer.service';
+import { CreateCostumerDto } from './dto/create-costumer.dto';
+import { UpdateCostumerDto } from './dto/update-costumer.dto';
+
+@Controller('costumer')
+export class CostumerController {
+  constructor(private readonly costumerService: CostumerService) {}
+
+  @Post()
+  create(@Body() createCostumerDto: CreateCostumerDto) {
+    return this.costumerService.create(createCostumerDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.costumerService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.costumerService.findOne(id);
+  }
+
+}
