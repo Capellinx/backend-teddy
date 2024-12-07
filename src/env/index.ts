8a77e339ed53b3671e745776849bfbdb0185377d
capellinx <capellaaa7@gmail.com>
2024-12-07 19:22:07 -0300

    feat: create variables of database

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,11 +2,11 @@ import { z } from 'zod'
 
 export const envSchema = z.object({
   PORT: z.coerce.number().optional().default(3333),
-  // DB_HOST: z.string(),
-  // DB_PORT: z.coerce.number().optional(),
-  // DB_USERNAME: z.string(),
-  // DB_PASSWORD: z.string(),
-  // DB_NAME: z.string(),
+  DB_HOST: z.string(),
+  DB_PORT: z.coerce.number().optional(),
+  DB_USERNAME: z.string(),
+  DB_PASSWORD: z.string(),
+  DB_NAME: z.string(),
 })
 
 export type Env = z.infer<typeof envSchema>
