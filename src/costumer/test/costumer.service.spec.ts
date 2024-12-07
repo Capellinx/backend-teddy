62b09ebd4ec55f9fabf45f0c4eacd30bc5df6456
capellinx <capellaaa7@gmail.com>
2024-12-07 20:59:42 -0300

    refactor: move to other folder

--- src/costumer/test/costumer.service.spec.ts
+++ src/costumer/test/costumer.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CostumerService } from './costumer.service';
+import { CostumerService } from '../costumer.service';
 
 describe('CostumerService', () => {
   let service: CostumerService;
