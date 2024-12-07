7fa6f89053236b552c9c391db64a6462fbd0d039
capellinx <capellaaa7@gmail.com>
2024-12-07 19:22:33 -0300

    feat: configure module to use typeorm on application

---
+++ src/db/db.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Env } from "../env";
+
+@Module({
+  imports: [TypeOrmModule.forRootAsync({
+    useFactory: async(config: ConfigService<Env, true>) => ({
+      type: "postgres",
+      host: config.get<string>("DB_HOST", { infer: true }),
+      port: config.get<number>("DB_PORT", {infer: true}),
+      username: config.get<string>("DB_USERNAME", {infer: true}),
+      password: config.get<string>("DB_PASSWORD", {infer: true}),
+      database: config.get<string>("DB_NAME", {infer: true}),
+      entities: [],
+      migrations: [__dirname + "/migrations/*.ts"],
+      synchronize: false
+    }),
+    inject: [ConfigService],
+  })],
+  providers: [],
+  controllers: [],
+})
+export class DbModule {}
