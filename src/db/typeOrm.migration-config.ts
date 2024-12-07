4c1a224b75894b84b1d30990ad327e16a3901c7c
capellinx <capellaaa7@gmail.com>
2024-12-07 19:57:35 -0300

    refactor: update form to get environments variables

---
+++ src/db/typeOrm.migration-config.ts
@@ -0,0 +1,24 @@
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { DataSource } from "typeorm";
+import { Env, envSchema } from "../env";
+
+ConfigModule.forRoot({
+  isGlobal: true,
+  validate: (env) => envSchema.parse(env),
+})
+
+const configService: ConfigService<Env, true> = new ConfigService();
+
+const dataSourceOptions = new DataSource({
+  type: "postgres",
+  host: configService.get<string>("DB_HOST", { infer: true }),
+  port: configService.get<number>("DB_PORT", {infer: true}),
+  username: configService.get<string>("DB_USERNAME", {infer: true}),
+  password: configService.get<string>("DB_PASSWORD", {infer: true}),
+  database: configService.get<string>("DB_NAME", {infer: true}),
+  entities: [],
+  migrations: [__dirname + "/migrations/*.ts"],
+  synchronize: false,
+});
+
+export default dataSourceOptions;
