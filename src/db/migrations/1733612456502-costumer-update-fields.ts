6ad2350730999aa8018e70b708354ddf73b97969
capellinx <capellaaa7@gmail.com>
2024-12-07 20:03:53 -0300

    feat: migartion to add created_at and updated_at on costumer table

---
+++ src/db/migrations/1733612456502-costumer-update-fields.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CostumerUpdateFields1733612456502 implements MigrationInterface {
+    
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            ALTER TABLE costumers
+            ADD COLUMN created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            ADD COLUMN updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+        `);
+    }
+    
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        // Remove as colunas created_at e updated_at
+        await queryRunner.query(`
+            ALTER TABLE costumers
+            DROP COLUMN IF EXISTS created_at,
+            DROP COLUMN IF EXISTS updated_at
+        `);
+    }
+}
