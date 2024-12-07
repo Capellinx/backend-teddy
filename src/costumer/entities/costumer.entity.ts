e26c7a8fde16c490d3dbcd453084613431892d5c
capellinx <capellaaa7@gmail.com>
2024-12-07 20:11:10 -0300

    feat: create entity costumer

---
+++ src/costumer/entities/costumer.entity.ts
@@ -0,0 +1,36 @@
+import { randomUUID } from "node:crypto";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+interface ICostumerProps {
+  name: string;
+  salary: number;
+  company: number;
+  created_at: string;
+  updated_at: string;
+}
+
+@Entity({ name: "costumers" })
+export class Costumer {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ type: "varchar" })
+  name: string;
+
+  @Column({ type: "integer" })
+  salary: number;
+
+  @Column({ type: "integer" })
+  company: number;
+
+  @Column({ type: "timestamp" })
+  created_at: Date;
+
+  @Column({ type: "timestamp" })
+  updated_at: Date;
+
+  constructor(props: ICostumerProps, id?: string) {
+    this.id = id ?? randomUUID();
+    Object.assign(this, props);
+  }
+}
