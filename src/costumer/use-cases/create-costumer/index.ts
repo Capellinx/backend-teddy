6c6085751935c7246ccd85706e321e57fa7c8797
capellinx <capellaaa7@gmail.com>
2024-12-07 20:59:31 -0300

    feat: create use case to register costumer

---
+++ src/costumer/use-cases/create-costumer/index.ts
@@ -0,0 +1,32 @@
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { CreateCostumerDto } from "../../dto/create-costumer.dto";
+import { Costumer } from "../../entities/costumer.entity";
+
+@Injectable()
+export class CreateCostumerUseCase {
+  
+  constructor(
+    @InjectRepository(Costumer)
+    private costumerRepository: Repository<Costumer>,
+  ) {}
+  
+  async execute(costumer: CreateCostumerDto) {
+    const isAlreadyExistCostumer = await this.costumerRepository.findOne({
+      where: {
+        name: costumer.name,
+        company: costumer.company,
+        salary: costumer.salary,
+      }
+    })
+    
+    if (isAlreadyExistCostumer) {
+      throw new BadRequestException("Costumer already exists");
+    }
+    
+    await this.costumerRepository.save(costumer);
+    
+    return
+  }
+}
