33d95d1d6256954b22724f07811ee4b07906326e
capellinx <capellaaa7@gmail.com>
2024-12-07 23:58:16 -0300

    feat: add new route to login in application

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -3,12 +3,14 @@ import { CreateAdminDto } from './domain/dto/create-admin.dto';
 import { Admin } from "./domain/entities/admin.entity";
 import { CreateAdminUseCase } from "./use-cases/create-admin";
 import { FindByNameUseCase } from "./use-cases/find-by-name";
+import { LoginAdminUseCase } from "./use-cases/login-admin";
 
 @Controller('admin')
 export class AdminController {
   constructor(
     private readonly createAdminUseCase: CreateAdminUseCase ,
     private readonly findByNameUseCase: FindByNameUseCase,
+    private readonly loginAdminUseCase: LoginAdminUseCase,
   ) {}
 
   @Post()
@@ -21,6 +23,14 @@ export class AdminController {
     @Query("name") name: string,
   ) {
     return this.findByNameUseCase.execute(name)
+  }
+  
+  @Post("login")
+  signIn(
+    @Body("username") username: string,
+    @Body("password") password: string,
+  ) {
+    return this.loginAdminUseCase.execute(username, password);
   }
 
 }
