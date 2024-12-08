344e45bcb7f5bca0318b69072895ca8906ecaef2
capellinx <capellaaa7@gmail.com>
2024-12-07 22:41:09 -0300

    feat: instance new module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from "@nestjs/config";
 import { envSchema } from "./env";
 import { DbModule } from './db/db.module';
 import { CostumerModule } from './costumer/costumer.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { CostumerModule } from './costumer/costumer.module';
     }),
     DbModule,
     CostumerModule,
+    AdminModule,
   ],
   controllers: [],
   providers: [],
