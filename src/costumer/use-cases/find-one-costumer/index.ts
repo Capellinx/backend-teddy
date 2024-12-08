704972da27928efb33230a3dd69b60bfcd39cd6b
capellinx <capellaaa7@gmail.com>
2024-12-07 21:17:38 -0300

    feat: create use case to find one user

---
+++ src/costumer/use-cases/find-one-costumer/index.ts
@@ -0,0 +1,32 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Costumer } from "../../entities/costumer.entity";
+
+
+@Injectable()
+export class FindOneCostumerUseCase {
+  
+  constructor(
+    @InjectRepository(Costumer)
+    private costumerRepository: Repository<Costumer>,
+  ) {}
+  
+  async execute(id: string): Promise<Costumer> {
+    const isAlreadyExistCostumer = await this.costumerRepository.findOne({
+      where: {
+        id
+      }
+    })
+    
+    if(!isAlreadyExistCostumer) {
+      throw new NotFoundException("Costumer not found");
+    }
+    
+    return await this.costumerRepository.findOneOrFail({
+      where: {
+        id
+      }
+    })
+  }
+}
