efba9a1cffea8cd542d88d0469b2e21ebab31d03
capellinx <capellaaa7@gmail.com>
2024-12-07 21:25:29 -0300

    feat: create use case to delete costumer

---
+++ src/costumer/use-cases/delete-costumer/index.ts
@@ -0,0 +1,28 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Costumer } from "../../entities/costumer.entity";
+
+@Injectable()
+export class DeleteCostumerUseCase {
+  constructor(
+    @InjectRepository(Costumer)
+    private costumerRepository: Repository<Costumer>,
+  ) {}
+
+  async execute(id: string): Promise<void> {
+    const foundCostumer = await this.costumerRepository.findOneOrFail({
+      where: {
+        id,
+      },
+    });
+
+    if (!foundCostumer) {
+      throw new NotFoundException(`Costumer not found: ${id}`);
+    }
+
+    await this.costumerRepository.delete(id);
+
+    return;
+  }
+}
