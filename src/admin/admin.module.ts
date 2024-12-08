8c5decb10051ce9bbd9441959f3a42e2f9b1365b
capellinx <capellaaa7@gmail.com>
2024-12-07 23:08:04 -0300

    feat: reference admin entity

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { AdminController } from './admin.controller';
+import { Admin } from "./domain/entities/admin.entity";
 import { CreateAdminUseCase } from "./use-cases/create-admin";
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Admin]),
+  ],
   controllers: [AdminController],
   providers: [
     CreateAdminUseCase
