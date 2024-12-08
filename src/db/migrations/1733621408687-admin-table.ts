911d46120e2b430dba430e942f4f1c50d3f5d2cd
capellinx <capellaaa7@gmail.com>
2024-12-07 22:34:23 -0300

    feat: create new migration to register admin

---
+++ src/db/migrations/1733621408687-admin-table.ts
@@ -0,0 +1,23 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AdminTable1733621408687 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`);
+    await queryRunner.query(`
+        CREATE TABLE adnin
+        (
+            id       uuid NOT NULL DEFAULT uuid_generate_v4(),
+            name     varchar(255),
+            password varchar(255),
+            created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+        )
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        DROP TABLE IF EXISTS admin;
+    `);
+  }
+}
