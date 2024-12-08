421bc0d7f17e480430d34d1cff072c48d67f911f
capellinx <capellaaa7@gmail.com>
2024-12-07 23:58:06 -0300

    feat: configure jwt module

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,18 +1,37 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from "@nestjs/config";
+import { JwtModule } from "@nestjs/jwt";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { Env } from "../env";
 import { AdminController } from './admin.controller';
 import { Admin } from "./domain/entities/admin.entity";
 import { CreateAdminUseCase } from "./use-cases/create-admin";
 import { FindByNameUseCase } from "./use-cases/find-by-name";
+import { LoginAdminUseCase } from "./use-cases/login-admin";
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Admin]),
+    JwtModule.registerAsync({
+      global: true,
+      imports: [],
+      useFactory: (configService: ConfigService<Env, true>) => ({
+        secret: configService.get<string>("JWT_SECRET", { infer: true }),
+        signOptions: {
+          expiresIn: Number(
+            configService.get<number>("JWT_EXPIRATION_TIME", { infer: true }),
+          ),
+        },
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AdminController],
   providers: [
     CreateAdminUseCase,
-    FindByNameUseCase
+    FindByNameUseCase,
+    LoginAdminUseCase
   ],
+  exports: [FindByNameUseCase],
 })
 export class AdminModule {}
