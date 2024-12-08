72f34ed468af1e52c6d07265c16e66f64d82ff5a
capellinx <capellaaa7@gmail.com>
2024-12-07 21:31:03 -0300

    feat: create use case to update costumer

---
+++ src/costumer/use-cases/update-costumer/index.ts
@@ -0,0 +1,36 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { UpdateCostumerDto } from "../../dto/update-costumer.dto";
+import { Costumer } from "../../entities/costumer.entity";
+
+
+@Injectable()
+export class UpdatedCostumerUseCase {
+  
+  constructor(
+    @InjectRepository(Costumer)
+    private costumerRepository: Repository<Costumer>,
+  ) {}
+  
+  async execute(id: string, costumer: UpdateCostumerDto): Promise<Costumer[]> {
+    const costumerFound = await this.costumerRepository.findOneOrFail({
+      where: {
+        id
+      }
+    })
+    
+    if (!costumerFound) {
+      throw new NotFoundException(`Costumer not found: ${id}`);
+    }
+    
+    const newCostumerInformation = new Costumer({
+      name: costumer.name,
+      salary: costumer.salary,
+      company: costumer.company,
+    })
+    
+    await this.costumerRepository.update(id, newCostumerInformation);
+    
+    return  }
+}
