f2343d573940c29bc13cb61e29ac9da4d91f29c7
capellinx <capellaaa7@gmail.com>
2024-12-07 23:07:49 -0300

    refactor: define entity class

--- src/admin/domain/entities/admin.entity.ts
+++ src/admin/domain/entities/admin.entity.ts
@@ -1,22 +1,22 @@
-import { Column, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 interface IAdminProps {
-  name: string,
-  password: string,
+  name: string;
+  password: string;
 }
 
+@Entity({ name: "admins" })
 export class Admin {
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn("uuid")
   id: string;
-  
-  @Column({ type: 'varchar', length: 255 })
+
+  @Column({ type: "varchar", length: 255 })
   name: string;
-  
-  @Column({ type: 'varchar', length: 255 })
+
+  @Column()
   password: string;
-  
+
   constructor(props: IAdminProps) {
     Object.assign(this, props);
   }
 }
-
