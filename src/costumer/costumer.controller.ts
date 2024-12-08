753157775c96615da539824bf56c14b6085c9788
capellinx <capellaaa7@gmail.com>
2024-12-07 21:59:46 -0300

    feat: implements new use case on controller

--- src/costumer/costumer.controller.ts
+++ src/costumer/costumer.controller.ts
@@ -1,25 +1,44 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { CostumerService } from './costumer.service';
-import { CreateCostumerDto } from './dto/create-costumer.dto';
-import { UpdateCostumerDto } from './dto/update-costumer.dto';
+import { CreateCostumerDto } from './domain/dto/create-costumer.dto';
+import { UpdateCostumerDto } from './domain/dto/update-costumer.dto';
+import { CreateCostumerUseCase } from "./use-cases/create-costumer";
+import { DeleteCostumerUseCase } from "./use-cases/delete-costumer";
+import { FindAllCostumersUseCase } from "./use-cases/find-all-costumers";
+import { FindOneCostumerUseCase } from "./use-cases/find-one-costumer";
+import { UpdatedCostumerUseCase } from "./use-cases/update-costumer";
 
 @Controller('costumer')
 export class CostumerController {
-  constructor(private readonly costumerService: CostumerService) {}
+  constructor(
+    private readonly createCostumerUseCase: CreateCostumerUseCase,
+    private readonly findOneCostumerUseCase: FindOneCostumerUseCase,
+    private readonly findAllCostumersUseCase: FindAllCostumersUseCase,
+    private readonly deleteCostumerUseCase: DeleteCostumerUseCase,
+    private readonly updatedCostumerUseCase: UpdatedCostumerUseCase,
+  ) {}
 
   @Post()
   create(@Body() createCostumerDto: CreateCostumerDto) {
-    return this.costumerService.create(createCostumerDto);
+    return this.createCostumerUseCase.execute(createCostumerDto);
   }
 
   @Get()
   findAll() {
-    return this.costumerService.findAll();
+    return this.findAllCostumersUseCase.execute();
   }
-
+  
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.costumerService.findOne(id);
+    return this.findOneCostumerUseCase.execute(id);
   }
 
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCostumerDto: UpdateCostumerDto) {
+    return this.updatedCostumerUseCase.execute(id, updateCostumerDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.deleteCostumerUseCase.execute(id);
+  }
 }
