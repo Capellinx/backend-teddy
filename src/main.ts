92fef050f187d9f9ba8ede2aa4d89fe092e9d0b6
capellinx <capellaaa7@gmail.com>
2024-12-08 11:32:05 -0300

    feat: configure swagger

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { ConfigService } from "@nestjs/config";
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from './app.module';
 import { Env } from "./env";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle("Manager control costumers")
+    .setTitle("The api to managers control your costumers")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup("api", app, document);
   
-  const config = app.get<ConfigService<Env, true>>(ConfigService);
-  const port = config.get<Env>('PORT', { infer: true});
   
   app.enableCors({
     origin: '*',
@@ -15,6 +22,9 @@ async function bootstrap() {
     allowedHeaders: '*',
   });
   
+  const config = app.get<ConfigService<Env, true>>(ConfigService);
+  const port = config.get<Env>('PORT', { infer: true});
+
   await app.listen(port);
 }
 bootstrap();
